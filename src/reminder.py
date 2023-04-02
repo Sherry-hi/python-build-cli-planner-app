878ed49570c6ca8547ddf9bd05d064cbc520c6ad
Sherry Fu <shuang.fu@sas.com>
2023-04-02 02:15:20 +0000

    a

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,10 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+    def PoliteReminder(PrefixedReminder):
+        def __init__(text):
+            super().__init__(prefix="please")
+
+
+        
